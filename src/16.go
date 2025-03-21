93544d8ea964a4f44026aa6eeb566777a2bc3a4c
jajsgenesitha <jajsgenesitha@gmail.com>
2025-03-21 06:50:21 +0000

    Add 16.go

---
+++ src/16.go
@@ -0,0 +1,17 @@
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+
+# Example usage:
+num = 360
+factors = prime_factors(num)
+print(f"Prime factors of {num} are: {factors}")
