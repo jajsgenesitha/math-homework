d6b007a4deeeba01f2b8e34cfde45c05bb5fd7a0
jajsgenesitha <jajsgenesitha@gmail.com>
2025-04-09 01:44:03 +0000

    Add 32.go

---
+++ src/32.go
@@ -0,0 +1,14 @@
+def fibonacci(n):
+    if n <= 0:
+        return "Invalid input"
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        a, b = 0, 1
+        for _ in range(2, n + 1):
+            a, b = b, a + b
+        return b
+
+print(fibonacci(5))
